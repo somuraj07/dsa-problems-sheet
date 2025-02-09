42a3246aa38b3927816e9d1bdfe58c530609b4b9
somu <somasankar33@gmail.com>
2025-02-09 12:43:44 +0530

    sum of two array

---
+++ arrays/problem-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        int sum(int arr[], int n) {
+            // Initialize sum to 0
+            int sum = 0;
+            // Iterate through each element in the array
+            for (int i = 0; i < n; i++) {
+                // Add each element to the sum
+                sum += arr[i];
+            }
+            return sum;
+        }
+    };
